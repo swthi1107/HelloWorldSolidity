90f8f9c23eb5af30a4e1e32131efb150f6dea5ea
swthi1107 <swathi.maddela11@gmail.com>
2026-03-27 22:41:41 +0530

    Initial commit

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+
+    string private helloMessage = "Hello world";
+
+    function getHelloMessage() public view returns (string memory) {
+        return helloMessage;
+    }
+
+}
